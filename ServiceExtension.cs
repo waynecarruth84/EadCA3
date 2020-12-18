1b5f089669a6f23b5cb50688035b28b762befb77
waynecarruth84 <waynecarruth@gmail.com>
2020-12-18 02:48:52 +0000

    Add files via upload

---
+++ ServiceExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+namespace EadCA3X00138115
+{
+    public static class ServiceExtension
+    {
+        public static async Task<T> GetJsonAsync<T>(this HttpClient httpClient, string url)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, url); //makes request
+
+
+            var response = await httpClient.SendAsync(request);
+            response.Headers.Add("Access-Control-Allow-Origin", "*"); //Allows for CORS in response headers
+            response.EnsureSuccessStatusCode();
+
+            var responseBytes = await response.Content.ReadAsByteArrayAsync();
+
+            return JsonSerializer.Deserialize<T>(responseBytes);
+        }
+    }
+}
